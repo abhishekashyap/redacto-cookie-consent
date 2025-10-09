1c16b862aa9f9210f38b47fe98a5759c43b4c696
Abhishek Kashyap <abhishek@redacto.io>
2025-10-10 00:10:11 +0530

    chore: cleanup

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { RedactoInstance } from "./types";
-declare const redacto: RedactoInstance;
-export default redacto;
-export { redacto };
-export * from "./types";
-//# sourceMappingURL=index.d.ts.map
